d1b1e4029e6616578ba8dbce081935262110e392
nuria.aquilue <nuria.aquilue@ctfc.cat>
2020-05-24 11:24:43 +0200

    sensanalysis

--- rscripts/01.play.model.r
+++ rscripts/01.play.model.r
@@ -35,8 +35,8 @@ source("mdl/define.scenario.r")
 source("mdl/land.dyn.mdl.r")
 wfactors <- read.table("scenarios/wfactors.txt", header=T)
 id.scn <- c(paste0("00", 1:9), paste0("0", 10:99), 100:286)
-rpb <- 0.2
-for(i in 1:286){
+nrun <- 3; rpb <- 0.9
+for(i in 206:232){
   scn.name <- paste0("Test", rpb*10, id.scn[i])
   ## Change weights of spread factors
   x <- unlist(filter(wfactors, scn==i) %>% select(-scn))
@@ -44,7 +44,7 @@ for(i in 1:286){
   write.table(sprdw, paste0("inputfiles/WeightSprdFactors_", rpb*10, id.scn[i], ".txt"), quote=F, sep="\t", row.names=F)
   define.scenario(scn.name)
   file.sprd.weight <- paste0("WeightSprdFactors_", rpb*10, id.scn[i])
-  dump(c("rpb", "file.sprd.weight"), paste0("outputs/", scn.name, "/scn.custom.def.r"))
+  dump(c("nrun", "rpb", "file.sprd.weight"), paste0("outputs/", scn.name, "/scn.custom.def.r"))
   land.dyn.mdl(scn.name)
 }
 
--- rscripts/03.sensanalysis.r
+++ rscripts/03.sensanalysis.r
@@ -1,41 +1,57 @@
 rm(list=ls())
 library(raster)
 library(tidyverse)
-load("inputlyrs/rdata/mask.rdata")
-load("inputlyrs/rdata/fireperim.89-99.rdata")
 id.scn <- c(paste0("00", 1:9), paste0("0", 10:99), 100:286)
-rpb <- 0.1
-i <- 1
+rpb <- 0.9
+nscn <- 205
 result <- data.frame(scn=NA, run=NA, year=NA, fire.id=NA, am=NA)
-for(i in 1:3){  #286
+for(i in 1:nscn){  
   scn.name <- paste0("Test", rpb*10, id.scn[i])
   for(r in 1:3){
+    load("inputlyrs/rdata/mask.89-99.rdata")
+    load("inputlyrs/rdata/fireperim.89-99.rdata")
     for(y in 1:11){
-      cat(paste)
+      cat(paste("Scn:", scn.name, "- run:", r, "- year:", y), "\n")
       perim.y <- perim.id[,c(1,y+1)]; names(perim.y)[2] <- "perim"
       FIRE <- raster(paste0("outputs/", scn.name, "/lyr/FireID_r", r, "t", y, ".tif"))
       dta <- data.frame(cell.id=1:ncell(MASK[]), mask=MASK[], fire.id=FIRE[])
-      dta$fire.id[is.na(dta$fire.id)] <- 0
       aux <- filter(dta, !is.na(mask)) %>% select(-mask) %>% left_join(perim.y, by="cell.id") %>%
-             filter(fire.id!=0 & perim!=0) %>% group_by(fire.id) %>% summarize(am=length(fire.id))
+             filter(!is.na(fire.id)) %>% mutate(dif=fire.id-perim) %>% filter(dif==0) %>% 
+             group_by(fire.id) %>% summarize(am=length(fire.id))
+      if(nrow(aux)>0)
+        result <- rbind(result, data.frame(scn=scn.name, run=r, year=y, aux))    
+    }
+    load("inputlyrs/rdata/mask.00-12.rdata")
+    load("inputlyrs/rdata/fireperim.00-12.rdata")
+    for(y in 12:24){
+      cat(paste("Scn:", scn.name, "- run:", r, "- year:", y), "\n")
+      perim.y <- perim.id[,c(1,y-10)]; names(perim.y)[2] <- "perim"
+      FIRE <- raster(paste0("outputs/", scn.name, "/lyr/FireID_r", r, "t", y, ".tif"))
+      dta <- data.frame(cell.id=1:ncell(MASK[]), mask=MASK[], fire.id=FIRE[])
+      aux <- filter(dta, !is.na(mask)) %>% select(-mask) %>% left_join(perim.y, by="cell.id") %>%
+             filter(!is.na(fire.id)) %>% mutate(dif=fire.id-perim) %>% filter(dif==0) %>% 
+             group_by(fire.id) %>% summarize(am=length(fire.id))
       if(nrow(aux)>0)
         result <- rbind(result, data.frame(scn=scn.name, run=r, year=y, aux))    
     }
   }
 }
+result <- result[-1,]
+
 
 ## Pctg of area match per fire and scenario
 fire.ignis <- read.table("inputfiles/FireIgnitions.txt", header=T)
-result <- result[-1,]
 report.fire <- left_join(result, fire.ignis, by=c("fire.id", "year")) %>%
-               mutate(pctg=am/area*100) %>% group_by(scn, year, fire.id, area, fst, wind) %>% 
-               summarise(pctg=round(mean(pctg),1))
-
+               mutate(pctg=am/area*100) %>% group_by(scn, year, fire.id, area, fst) %>% 
+               summarise(pctg=round(mean(pctg),1), am=round(mean(am),1))
+write.table(report.fire, paste0("rscripts/outs/ReportAreaMatchFire_0", rpb*10, ".txt"), quote=F, row.names=F, sep="\t")
+            
 ## Pctg of area match per scenario and fire.spread.type
 report.scn <- group_by(report.fire, scn, fst) %>% 
-              summarise(min.match=min(pctg), mn.match=mean(pctg), max.match=max(pctg))
+              summarise(min.match=min(pctg), mn.match=round(mean(pctg),1), max.match=max(pctg))
+write.table(report.scn, paste0("rscripts/outs/ReportAreaMatchScn_0", rpb*10, ".txt"), quote=F, row.names=F, sep="\t")
 
 ## Best scn per fire spread type
 scn.names <- paste0("Test", rpb*10, id.scn)
 group_by(report.scn, fst) %>% summarise(best=which.max(mn.match)) %>%
-  mutate(scn=scn.names[best])
+    mutate(scn=scn.names[best])
